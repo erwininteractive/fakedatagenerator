f8816945cf6a6171489aff489ea065aadbd53c0c
Andrew S Erwin <andrew@erwininteractive.com>
2023-03-28 08:56:15 -0500

    add enums definitions

--- src/Generator.ts
+++ src/Generator.ts
@@ -1,4 +1,5 @@
 import validFormatters from './data/formatters.json'
+import { Gender } from './enums'
 
 type formatter = keyof typeof validFormatters
 
@@ -19,5 +20,15 @@ export default class Generator
         const index: number = Math.floor(Math.random() * validFormatters[f].length)
 
         return validFormatters[f][index] as string
+    }
+
+    public Name(gender?: Gender, title?: boolean, suffix?: boolean): string {
+        let g: formatter = null
+
+        if (typeof gender !== 'undefined') {
+            g = gender == Gender.Male ? 'firstNameMale' : 'firstNameFemale'
+        }
+
+        return this.generateText(g)
     }
 }
