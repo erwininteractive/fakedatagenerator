34f91feb4635a2f95412633221184f1786a3f890
Andrew S Erwin <andrew@erwininteractive.com>
2023-04-06 09:17:02 -0500

    add person methods - remove format method

--- src/Generator.ts
+++ src/Generator.ts
@@ -6,39 +6,67 @@ type formatter = keyof typeof validFormatters
 
 export default class Generator
 {
-    public format(f: keyof typeof validFormatters): string {
-        const formatter: formatter = f
+    private generateText(f: formatter): string {
         const valid: boolean = Object.keys(validFormatters).includes(f)
 
-        if (!valid) {
+        if(!valid) {
             throw new Error(`${f} is not a valid formatter`)
         }
 
-        return this.generateText(formatter)
-    }
-
-    private generateText(f: formatter): string {
         const index: number = Math.floor(Math.random() * validFormatters[f].length)
 
         return validFormatters[f][index] as string
     }
 
-    public Name(gender?: Gender, title?: boolean, suffix?: boolean): string {
+    public FirstName(gender?: Gender): string {
+        gender = gender || randomElement(Gender)
+        
+        let name: String = new String
+
+        name = name.concat(gender == Gender.Male ? this.generateText('firstNameMale') : this.generateText('firstNameFemale'))
+
+        return name.toString()
+    }
+
+    public LastName(): string {
+        return this.generateText('lastName')
+    }
+
+    public Title(gender?: Gender): string {
+        gender = gender || randomElement(Gender)
+
+        let title: String = new String
+
+        title = title.concat(gender == Gender.Male ? this.generateText('titleMale') : this.generateText('titleMale'))
+
+        return title.toString()
+    }
+
+    public Suffix(gender?: Gender): string {
+        gender = gender || randomElement(Gender)
+
+        let suffix: String = new String
+        
+        suffix = suffix.concat(gender == Gender.Male? this.generateText('suffixMale') : this.generateText('suffixFemale'))
+
+        return suffix.toString()
+    }
+
+    public FullName(gender?: Gender, title?: boolean, suffix?: boolean): string {
         gender = gender || randomElement(Gender)
 
         let name: String = new String
-        name = name.concat(gender + ' | ')
 
         if (title) {
-            name = name.concat(gender == Gender.Male ? this.generateText('titleMale') : this.generateText('titleFemale'))
+            name = name.concat(this.Title(gender))
             name = name.concat(' ')
         }
 
-        name = name.concat(gender == Gender.Male ? this.generateText('firstNameMale') : this.generateText('firstNameFemale'))
-        name = name.concat(' ' + this.generateText('lastName'))
+        name = name.concat(this.FirstName(gender))
+        name = name.concat(' ' + this.LastName())
 
         if (suffix) {
-            name = name.concat(', ' + this.generateText('suffix'))
+            name = name.concat(', ' + this.Suffix(gender))
         }
 
         return name.toString()
--- src/functions.ts
+++ src/functions.ts
@@ -1,14 +1,4 @@
-export function randomElement<T>(element: T): T[keyof T] {
-    const values = Object.keys(element)
-        .map(n => Number.parseInt(n))
-        .filter(n => !Number.isNaN(n)) as unknown as T[keyof T][]
-
-    const index = Math.floor(Math.random() * values.length)
-
-    return values[index]
-}
-
-export function getRandomEnum(e) {
+export function randomElement(e) {
     const values = Object.keys(e);
     const key = values[Math.floor(Math.random() * values.length)]
 
